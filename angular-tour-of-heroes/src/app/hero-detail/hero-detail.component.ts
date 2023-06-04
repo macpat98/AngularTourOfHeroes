e4b20f4087d39cf0f7df81edbad3f11f381c010e
pat.maciejewski98@gmail.com <pat.maciejewski98@gmail.com>
2023-06-04 17:51:36 +0200

    adding add,delete,update and search heroes features

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -16,19 +16,24 @@ export class HeroDetailComponent implements OnInit {
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ) {};
+  ) {}
 
   ngOnInit(): void {
     this.getHero();
-  };
+  }
 
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero);
-  };
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    }
   }
 }
